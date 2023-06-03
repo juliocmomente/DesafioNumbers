ccbc60517d9c1a2ee4396354a40b9d302c3e6921
Julio Cesar Momente <juliocesar.momente@gmail.com>
2023-06-02 21:33:47 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Desafio área do círculo");
+
+        double raio = 1.0;
+        System.Console.Write("Digite o valor do raio: ");
+        raio = Convert.ToDouble(System.Console.ReadLine());
+
+
+        double area = Math.PI * raio * raio;
+
+        System.Console.WriteLine($"A área do círculo de raio {raio} é {area}");
+    }
+}
